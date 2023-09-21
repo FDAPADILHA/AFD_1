d9dcd4d460d5b208d8f37cb6a8636f0c8f0bbd9e
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-09-21 02:02:40 -0300

    defining the constructor and creating functios

--- src/afd_1/AFD_1.java
+++ src/afd_1/AFD_1.java
@@ -1,11 +1,28 @@
 
 package afd_1;
 
+import java.util.Scanner;
+
 public class AFD_1 {
     int COUNT ;
     char PALAVRA[];
     public static void main(String[] args) {
+        AFD_1 AUT = new AFD_1();
+        String SENTENCA;
+        Scanner ENTRADA = new Scanner(System.in);
+        System.out.println("Insira a entrada para o Autômato: ");
+        SENTENCA = ENTRADA.nextLine();
+        AUT.Iniciar();
+   }
+    public void Iniciar(){ }
+    public void q0(){}
+    public void q1(){}
+    public void q2(){} 
+    public void q3(){}
+    public void qf(){}
+    public void qerro(){}
+        
+    
        
-    }
     
 }
