dedbfe5152b57c94ff0f3f12a78ab738acd765be
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-09-21 01:53:03 -0300

    repository testing

--- src/afd_1/AFD_1.java
+++ src/afd_1/AFD_1.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package afd_1;
 
-/**
- *
- * @author XPS
- */
 public class AFD_1 {
-
-    /**
-     * @param args the command line arguments
-     */
+    int COUNT ;
+    char PALAVRA[];
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
